f4a1bab9706dc8490fadc3390075b6ed0079468e
likamrat <likamrat@microsoft.com>
2022-05-04 21:46:36 -0700

    v1.6.0_2022-05-02

--- azure_arc_data_jumpstart/cluster_api/capi_azure/ARM/artifacts/DataServicesLogonScript.ps1
+++ azure_arc_data_jumpstart/cluster_api/capi_azure/ARM/artifacts/DataServicesLogonScript.ps1
@@ -19,7 +19,7 @@ az config set extension.use_dynamic_install=yes_without_prompt
 # Installing Azure CLI extensions
 Write-Host "`n"
 Write-Host "Installing Azure CLI extensions"
-az extension add --name arcdata
+az extension add --name arcdata --system
 az extension add --name connectedk8s
 az extension add --name k8s-extension
 Write-Host "`n"
